147874738eef37b7c3fe71dca0941d35693bd3b1
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2021-08-18 20:32:13 +0530

    intl footer date.

--- components/AppFooter.js
+++ components/AppFooter.js
@@ -11,7 +11,11 @@ export const AppFooter = () => {
     return (
         <footer className="my-8 text-center">
             <p className="mt-3">
-                {t("lastUpdated", {date: intl.formatDateTime(new Date())})}
+                {t("lastUpdated", {date: intl.formatDateTime(new Date(),  {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                })})}
             </p>
             <a target="_blank" href="https://github.com/nuuuwan" className="mt-3 text-sm hover:text-blue-600 text-blue-500">
                 {t("dataBy")}
